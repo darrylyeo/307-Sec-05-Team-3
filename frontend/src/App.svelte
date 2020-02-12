47cc4b83fcea43bc87540b4829c90d57ac68db68
Darryl Yeo <dev@darryl-yeo.com>
2020-02-11 16:22:36 -0800

    Add Svelte frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let name;
+</script>
+
+<header>
+	<nav>
+		<h1>CampusNow</h1>
+		<button>Log In</button>
+	</nav>
+</header>
+<main>
+	
+</main>
+
+<style>
+	:root {
+		font-family: sans-serif;
+	}
+
+	* {
+		box-sizing: border-box;
+		margin: 0;
+	}
+
+	div,
+	header, main, footer, nav {
+		display: grid;
+	}
+</style>
