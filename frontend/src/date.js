3e800a9cd157e25e70658c7f12ee9af6e7a9945c
Darryl Yeo <dev@darryl-yeo.com>
2020-02-25 08:17:41 +0000

    Better date format

--- frontend/src/date.js
+++ frontend/src/date.js
@@ -1,5 +1,9 @@
 const formatDate = dateString => {
-	return new Date(dateString).toLocaleString()
+	return new Date(dateString).toLocaleString('en-US', {
+		dateStyle: 'medium',
+		timeStyle: 'short'
+	})
+	// .toLocaleString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' })
 }
 
 export {
